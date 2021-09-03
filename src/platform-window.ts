3d485d0a3596f972f927ecc74119c6dabee03a0f
wenjunche <wenjun@openfin.co>
2021-09-02 20:51:54 -0400

    css loader

--- src/platform-window.ts
+++ src/platform-window.ts
@@ -1,3 +1,6 @@
+import '../public/frame-styles.css';
+import '../public/light-theme.css';
+
 export const CONTAINER_ID = 'layout-container';
 window.addEventListener('DOMContentLoaded', () => {
     fin.Platform.Layout.init({containerId: CONTAINER_ID});
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
             }
         ]
     },
